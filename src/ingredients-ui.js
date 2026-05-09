d0f5b0f24e06864cb11b9185acd0ae84ea15654a
MCurb <curbecomarcos.dev@gmail.com>
2026-05-08 22:10:31 -0600

    feat: capture ingredient info from UI and create ingredient objects

---
+++ src/ingredients-ui.js
@@ -0,0 +1,24 @@
+import { ingredientsClass } from './storage/ingredients';
+
+// ========================
+// DOM REFERENCES (static)
+// ========================
+
+const addIngBtn = document.querySelector('.add-ing-btn');
+const ingName = document.querySelector('.ing-name');
+const ingPrice = document.querySelector('.ing-price');
+const ingQuantity = document.querySelector('.ing-quantity');
+const ingUnit = document.querySelector('.ing-unit');
+
+addIngBtn.addEventListener('click', () => {
+  const newIngredient = {
+    id: crypto.randomUUID(),
+    name: ingName.value,
+    stockPrice: Number(ingPrice.value),
+    quantity: Number(ingQuantity.value),
+    unit: ingUnit.value,
+  };
+
+  ingredientsClass.addIngredient(newIngredient);
+  console.log(ingredientsClass.getIngredient(newIngredient.id));
+});
