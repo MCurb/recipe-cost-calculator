caca51c661ee61802d35020a105d83b9985e070c
MCurb <curbecomarcos.dev@gmail.com>
2026-05-11 16:13:47 -0600

    fix: update recipe object structure

---
+++ src/storage/recipes.js
@@ -0,0 +1,48 @@
+class Recipes {
+  constructor() {
+    this.recipes = [
+      {
+        id: crypto.randomUUID(),
+        name: 'Pastel de Zanahoria',
+        ingredientsUsed: [
+          {
+            ingInfo: {
+              id: 'ing id',
+              name: 'Harina',
+              stockPrice: 24,
+              quantity: 500,
+              unit: 'grams',
+            },
+            recipeUse: {
+              quantityUsed: 1,
+              unitUsed: 'kilograms',
+              pricePerUnit: 48,
+              ingPriceUsed: 48,
+            },
+          },
+        ],
+        recipeCost: 48,
+      },
+    ];
+  }
+
+  getRecipesData() {
+    return this.recipes;
+  }
+
+  getRecipe(id) {
+    return this.recipes.filter((recipe) => recipe.id === id)[0];
+  }
+
+  addRecipe(obj) {
+    this.recipes.push(obj);
+  }
+
+  removeRecipe(id) {
+    this.recipes = this.recipes.filter((recipe) => {
+      recipe.id !== id;
+    });
+  }
+}
+
+export const recipesClass = new Recipes();
