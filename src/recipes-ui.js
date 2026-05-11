cdd7431a1b92c1693fb60be5c1fcada88a723bc4
MCurb <curbecomarcos.dev@gmail.com>
2026-05-11 16:11:59 -0600

    feat: add event listeners to add recipes with ingredients

--- src/recipes-ui.js
+++ src/recipes-ui.js
@@ -1,3 +1,4 @@
+import convert from 'convert';
 import { observer } from './observer';
 import { ingredientsClass } from './storage/ingredients';
 import { recipesClass } from './storage/recipes';
@@ -9,6 +10,8 @@ import { recipesClass } from './storage/recipes';
 const addIngToRecipeBtn = document.querySelector('.add-ing-recipe-btn');
 const addRecipeBtn = document.querySelector('.add-recipe-btn');
 const recipeName = document.querySelector('.recipe-name');
+const ingQuantityUsed = document.querySelector('.recipe-ing-quantity');
+const ingUnitUsed = document.querySelector('.ing-unit-used');
 
 const selectIng = document.querySelector('.select-ingredient');
 
@@ -30,3 +33,49 @@ function updateIngSelect(ingredientsObj) {
 
 observer.subscribe(updateIngSelect);
 observer.notify(ingredientsClass.getIngredientsData());
+
+let pendingIng = [];
+
+addIngToRecipeBtn.addEventListener('click', () => {
+  const ingUsedId = selectIng.selectedOptions[0].dataset.id;
+  const ingObj = ingredientsClass.getIngredient(ingUsedId);
+  const { stockPrice, quantity, unit } = ingObj;
+
+  const ingUsed = {
+    ingInfo: ingObj,
+    recipeUse: {
+      quantityUsed: ingQuantityUsed.value,
+      unitUsed: ingUnitUsed.value,
+    },
+  };
+
+  const priceUnit =
+    stockPrice / convert(quantity, unit).to(ingUsed.recipeUse.unitUsed);
+  ingUsed.recipeUse.pricePerUnit = priceUnit;
+
+  const priceUsed =
+    ingUsed.recipeUse.quantityUsed * ingUsed.recipeUse.pricePerUnit;
+  ingUsed.recipeUse.ingPriceUsed = priceUsed;
+
+  pendingIng.push(ingUsed);
+});
+
+addRecipeBtn.addEventListener('click', () => {
+  const newRecipe = {
+    id: crypto.randomUUID(),
+    name: recipeName.value,
+    ingredientsUsed: [],
+    recipeCost: 0,
+  };
+
+  pendingIng.forEach((ingredient) => {
+    newRecipe.ingredientsUsed.push(ingredient);
+  });
+
+  newRecipe.ingredientsUsed.forEach((ingredient) => {
+    newRecipe.recipeCost += ingredient.recipeUse.ingPriceUsed;
+  });
+
+  recipesClass.addRecipe(newRecipe);
+  console.log(recipesClass.getRecipe(newRecipe.id));
+});
