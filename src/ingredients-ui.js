9de2967fe45ae508bb0412544e24ee669c39b97a
MCurb <curbecomarcos.dev@gmail.com>
2026-05-13 15:28:44 -0600

    refactor: clean commented code

--- src/ingredients-ui.js
+++ src/ingredients-ui.js
@@ -48,58 +48,6 @@ function createIngCardUI(ingredientObj) {
   return ingCard;
 }
 
-// function createIngCardUI(ingredient) {
-//   const { id, name, stockPrice, quantity, unit } = ingredient;
-
-//   const ingNameActions = createNameActions(id, name);
-//   const ingInfo = createIngInfo(stockPrice, quantity, unit);
-
-//   const ingCard = createElement('div', 'ingredient-card');
-//   ingCard.append(ingNameActions, ingInfo);
-
-//   return ingCard;
-// }
-
-// function createNameActions(id, name) {
-//   const recipeName = createElement('h3');
-//   recipeName.textContent = name;
-
-//   const editBtn = createElement('button', 'edit-ing-btn');
-//   editBtn.textContent = 'Edit';
-//   const deleteBtn = createElement('button', 'del-ing-btn');
-//   deleteBtn.textContent = 'Delete';
-
-//   [editBtn, deleteBtn].forEach((button) => (button.dataset.ingId = id));
-
-//   const buttons = createElement('div', 'buttons');
-//   buttons.append(editBtn, deleteBtn);
-
-//   const nameActionsCont = createElement('div', 'ing-name-actions-container');
-//   nameActionsCont.append(recipeName, buttons);
-//   return nameActionsCont;
-// }
-
-// function createIngInfo(stockPrice, quantity, unit) {
-//   const cost = createElement('p', 'ing-price-ui');
-//   cost.textContent = `Cost: ${stockPrice} MXN`;
-
-//   const quantityUnit = createElement('p', 'ing-quantity-unit');
-//   quantityUnit.textContent = `Unit: ${quantity} ${quantity > 1 ? unit : unit.slice(0, -1)}`;
-
-//   const ingInfoCont = createElement('div', 'ing-info-container');
-//   ingInfoCont.append(quantityUnit, cost);
-
-//   return ingInfoCont;
-// }
-
-// export function createElement(element, className) {
-//   const elem = document.createElement(element);
-//   if (className) elem.classList.add(className);
-//   return elem;
-// }
-
-// const ingCardBtnContainer = document.querySelector('.ingredient-card .buttons');
-
 // ingCardBtnContainer.addEventListener('click', (e) => {
 //   if (e.target.classList.contains('edit-ing-btn')) {
 //     const ingredientObj = ingredientsClass.getIngredient(
